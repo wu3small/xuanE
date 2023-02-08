0e99ba5ece487310db2e1a46c44afba87c85c699
wu3small <123550365+wu3small@users.noreply.github.com>
2023-02-08 08:49:48 +0800

    20230208A

---
+++ js/goToHome-wp.js
@@ -0,0 +1,7 @@
+homeButton ='<a href="../index.html" style="align-items: center;">\
+<div class="button01 cool02" style="bottom: 20px; left: 50%; opacity: 0.5; transform: translate(-50%, 0);">\
+    <p>回首頁</p>\
+</div>\
+</a>';
+
+document.write('homeButton')
