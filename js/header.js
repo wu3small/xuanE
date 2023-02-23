d7e812d91a73cf9d1814a9813e42480dc02216b0
wu3small <123550365+wu3small@users.noreply.github.com>
2023-02-23 22:21:13 +0800

    20230223BB

--- js/header.js
+++ js/header.js
@@ -1,13 +1,16 @@
-header ='<div class="title02boxes">\
-<div class="title02a">\
-米干網\
-</div>\
-<div class="title02b">\
-回首頁\
-</div>\
-<div class="title02b">\
-登入\
-</div>\
-</div>';
+let homeURL = location.protocol + location.host
+console.log(homeURL)
+
+let header ='<div class="title02boxes">'+
+'<div class="title02c"></div>'+
+'<div class="title02a">米干網</div>'+
+'<div class="title02c"></div>'+
+'<div class="title02b" onclick="history.back()">上一頁</div>'+
+'<div class="title02c"></div>'+
+'<div class="title02b" onclick="'+homeURL+'">回首頁</div>'+
+'<div class="title02c"></div>'+
+'<div class="title02b" onclick="'+homeURL+'">登入！</div>'+
+'<div class="title02c"></div>'+
+'</div>';
 
 document.write(header);
